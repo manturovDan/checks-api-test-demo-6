ad006d1636ceda05cfbd07ab8f93e84fe4e710a0
Danila Manturov <47337371+manturovDan@users.noreply.github.com>
2024-05-27 12:26:45 +0200

    Update RecTest.java fix 2

--- src/test/java/RecTest.java
+++ src/test/java/RecTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RecTest {
     @Test
     public void testTest() {
-        assertEquals(7, 8);
+        assertEquals(7, 7);
     }
 
     @Test
