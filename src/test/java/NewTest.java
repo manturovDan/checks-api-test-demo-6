6aff8fa34df71350a2b68b2fc790d26d55ce2998
Danila.Manturov <danila.manturov@jetbrains.com>
2024-09-02 14:05:26 +0200

    more new failures

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -211,4 +211,34 @@ public class NewTest {
     public void bigintTestNew1113() {
         assertEquals(14930352, Fibo.fibo_recursion(2));
     }
+
+
+    @Test
+    public void bigintTestNew1007x() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1008x() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1009x() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNewnew1110x() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew111100x() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1112x() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1113x() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
 }
