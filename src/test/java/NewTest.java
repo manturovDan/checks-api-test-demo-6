7ab51ab48281f0a8397636cb7e606ce570ef13e8
Danila.Manturov <danila.manturov@jetbrains.com>
2024-09-02 13:06:26 +0200

    add failed tests

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,26 @@
+import org.example.Fibo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RecTest {
+    @Test
+    public void testTest() {
+        assertEquals(7, 10);
+    }
+
+    @Test
+    public void firstTest() {
+        assertEquals(1, Fibo.fibo_recursion(11));
+    }
+
+    @Test
+    public void secondTest() {
+        assertEquals(1, Fibo.fibo_recursion(12));
+    }
+
+    @Test
+    public void bigintTest() {
+        assertEquals(14930352, Fibo.fibo_recursion(316));
+    }
+}
