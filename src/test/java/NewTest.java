276bd405eeaddde3c7d0675e3c87ffcd3921b3f3
Danila.Manturov <danila.manturov@jetbrains.com>
2024-09-02 13:49:26 +0200

    add a lot of tests

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -33,4 +33,182 @@ public class NewTest {
     public void bigintTestNew1() {
         assertEquals(14930352, Fibo.fibo_recursion(2));
     }
+
+    @Test
+    public void bigintTestNew1() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+
+    @Test
+    public void bigintTestNew1() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew2() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew3() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew4() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew5() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew6() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew7() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew8() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew9() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew10() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew11() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew12() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew13() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew14() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew15() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew16() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew17() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew18() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew19() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew20() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew101() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew102() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew103() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew104() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew105() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew106() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew107() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew108() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew109() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1110() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1001() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1002() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1003() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1004() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1005() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1006() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1007() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1008() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1009() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1110() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1111() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1112() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
+    @Test
+    public void bigintTestNew1113() {
+        assertEquals(14930352, Fibo.fibo_recursion(2));
+    }
 }
